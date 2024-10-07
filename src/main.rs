918718a8f297af336bba5c8d774044c31b0a59e4
Håkon Sunde <hsu@eksfin.no>
2024-10-07 10:35:50 +0200

    Add POST logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use serde::Deserialize;
-use reqwest;
+use serde::{Deserialize, Serialize};
+use reqwest::{self, StatusCode};
+use std::io::{self, Write};
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Blog {
-    _id: String,
+    _id: Option<String>,
     title: String,
     author: String,
     content: String,
@@ -14,10 +15,26 @@ type BlogsResponse = Vec<Blog>;
 
 fn get_blogs() -> Result<Vec<Blog>, Box<dyn std::error::Error>> {
     let url = format!("https://portfolioapi-hysa.onrender.com/Blogs");
-
     let response = reqwest::blocking::get(&url)?.json::<BlogsResponse>()?;
     
     Ok(response)
+}
+
+fn add_blog(blog: Blog) -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://portfolioapi-hysa.onrender.com/Blogs";
+    let client = reqwest::blocking::Client::new();
+    let response = client.post(url)
+        .json(&blog)
+        .send()?;
+
+        if response.status() == StatusCode::OK {
+            println!("Blog added successfully!");
+        } else {
+            println!("Failed to add blog. Status: {:?}", response.status());
+            println!("Response Body: {:?}", response.text()?);
+        }
+
+    Ok(())
 }
 
 fn main() {
@@ -32,5 +49,37 @@ fn main() {
             }
         },
         Err(e) => eprintln!("Error fetching blogs: {}", e),
+    }
+
+    let mut title = String::new();
+    let author = "Håkon Sunde";
+    let mut content = String::new();
+    let mut date = String::new();
+
+    print!("Enter title: ");
+    io::stdout().flush().unwrap(); 
+    io::stdin().read_line(&mut title).unwrap();
+    title = title.trim().to_string(); 
+
+    print!("Write blog content: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut content).unwrap();
+    content = content.trim().to_string();
+
+    print!("Enter blog date (YYYY-MM-DD): ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut date).unwrap();
+    date = date.trim().to_string();
+
+    let new_blog = Blog {
+        _id: None,  
+        title,
+        author: author.to_string(),
+        content,
+        date,
+    };
+
+    if let Err(e) = add_blog(new_blog) {
+        eprintln!("Error adding blog: {}", e);
     }
 }

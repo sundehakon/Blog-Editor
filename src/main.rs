b4f8b820b72417c3c1ad365fb18fbc9088f66f04
Håkon Sunde <hsu@eksfin.no>
2024-10-07 14:37:52 +0200

    Implement delete function

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,42 @@ struct Blog {
 type BlogsResponse = Vec<Blog>;
 
 fn get_blogs() -> Result<Vec<Blog>, Box<dyn std::error::Error>> {
-    let url = format!("https://portfolioapi-hysa.onrender.com/Blogs");
-    let response = reqwest::blocking::get(&url)?.json::<BlogsResponse>()?;
+    let url = "https://portfolioapi-hysa.onrender.com/Blogs";
+    let response = reqwest::blocking::get(url)?.json::<BlogsResponse>()?;
     
     Ok(response)
+}
+
+fn delete_blog(blog_id: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("https://portfolioapi-hysa.onrender.com/Blogs/{}", blog_id);
+
+    let mut username = String::new();
+    let mut password = String::new();
+
+    print!("Enter username: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut username).unwrap();
+    let username = username.trim();
+
+    print!("Enter password: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut password).unwrap();
+    let password = password.trim();
+
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .delete(&url)
+        .basic_auth(username, Some(password))
+        .send()?;
+
+    if response.status() == StatusCode::OK {
+        println!("Blog deleted successfully!");
+    } else {
+        println!("Failed to delete blog. Status: {:?}", response.status());
+        println!("Response Body: {:?}", response.text()?);
+    }
+
+    Ok(())
 }
 
 fn add_blog(blog: Blog) -> Result<(), Box<dyn std::error::Error>> {
@@ -58,9 +90,10 @@ fn main() {
 
     loop {
         println!("Choose an option:");
-        println!("1. Add a new blog");
+        println!("1. Create a new blog");
         println!("2. View all blogs");
-        println!("3. Exit");
+        println!("3. Delete blog by selecting number");
+        println!("4. Exit");
 
         let mut choice = String::new();
         io::stdin().read_line(&mut choice).expect("Failed to read line");
@@ -114,10 +147,33 @@ fn main() {
                 }
             }
             "3" => {
+                match get_blogs() {
+                    Ok(blogs) => {
+                        for (index, blog) in blogs.iter().enumerate() {
+                            println!("{}. {} by {} on {}", index + 1, blog.title, blog.author, blog.date);
+                        }
+                        println!("Enter the number of the blog to delete:");
+                        let mut delete_choice = String::new();
+                        io::stdin().read_line(&mut delete_choice).unwrap();
+                        let delete_choice: usize = delete_choice.trim().parse().unwrap_or(0);
+
+                        if delete_choice > 0 && delete_choice <= blogs.len() {
+                            let blog_id = &blogs[delete_choice - 1]._id.as_deref().unwrap();
+                            if let Err(e) = delete_blog(blog_id) {
+                                eprintln!("Error deleting blog: {}", e);
+                            }
+                        } else {
+                            println!("Invalid selection.");
+                        }
+                    },
+                    Err(e) => eprintln!("Error fetching blogs: {}", e),
+                }
+            }
+            "4" => {
                 println!("Goodbye!");
                 break;
             }
-            _ => println!("Invalid option. Please try again.")
+            _ => println!("Invalid option. Please try again."),
         }
     }
 }

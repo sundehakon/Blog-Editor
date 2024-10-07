1a3eb13d8acbb4ffc01870a4381a839433f82a6a
Håkon Sunde <hsu@eksfin.no>
2024-10-07 09:48:30 +0200

    Add API read logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use serde::Deserialize;
+use reqwest;
+
+#[derive(Deserialize, Debug)]
+struct Blog {
+    _id: String,
+    title: String,
+    author: String,
+    content: String,
+    date: String,
+}
+
+type BlogsResponse = Vec<Blog>;
+
+fn get_blogs() -> Result<Vec<Blog>, Box<dyn std::error::Error>> {
+    let url = format!("https://portfolioapi-hysa.onrender.com/Blogs");
+
+    let response = reqwest::blocking::get(&url)?.json::<BlogsResponse>()?;
+    
+    Ok(response)
+}
+
 fn main() {
-    println!("Hello, world!");
+    match get_blogs() {
+        Ok(blogs) => {
+            for blog in blogs {
+                println!("Title: {}", blog.title);
+                println!("Author: {}", blog.author);
+                println!("Content: {}", blog.content);
+                println!("Date: {}", blog.date);
+                println!("----------------------");
+            }
+        },
+        Err(e) => eprintln!("Error fetching blogs: {}", e),
+    }
 }
